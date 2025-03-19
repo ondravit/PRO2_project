367582200cfde1528c745deb6cf522419d804b92
ondravit <ondra.vit04@seznam.cz>
2025-03-19 14:23:09 +0100

    Initial commit

---
+++ src/main/java/com/example/pro2_project/Pro2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pro2_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro2ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pro2ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pro2_project/Pro2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pro2_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro2ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
